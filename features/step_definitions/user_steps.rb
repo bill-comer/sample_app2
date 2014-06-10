ae46bc278e9210a3e73b7bf37db43929b607c9c2
U-FORMFILL\comerb <comerb@UT-NB-215.formfill.co.uk>
2014-06-10 12:07:47 +0100

    users show test was not picking up correct email text

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,9 +1,6 @@
 #require 'test/factories/factories.rb'
 require 'factory_girl_rails'
 
-#def user
-#  @user ||= Factory :user
-#end
 
 Given /^I am an existing user$/ do
   @new_user = FactoryGirl.create(:user)
