ab2961621a346412bba634e671215d03b368b96c
Bill Comer <bill.comer@gmail.com>
2014-06-15 13:05:49 +0100

    length restrictions to microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,6 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
+  
+  validates :content, presence: true, length: { maximum: 140 , minimum: 1}
 end
